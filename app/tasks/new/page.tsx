8ce9daba1743287ac086ec2613564f6ac057891f
maxvdkelft0 <m.van.de.kelft@sintlucasedu.nl>
2023-12-05 12:09:09 +0100

    Add a markdown text editor

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -1,14 +1,14 @@
 'use client'
-import React from 'react'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-
+import { Button, TextField } from '@radix-ui/themes'
+import SimpleMDE from 'react-simplemde-editor'
+import "easymde/dist/easymde.min.css"
 const NewTaskPage = () => {
   return (
     <div className='max-w-xl space-y-3'>
         <TextField.Root>
             <TextField.Input placeholder='Title'/>
         </TextField.Root>
-        <TextArea placeholder='Description'/>
+        <SimpleMDE placeholder='Description'/>
         <Button>Submit New Task</Button>
     </div>
   )
